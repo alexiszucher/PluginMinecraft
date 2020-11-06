53836d6dcab03caf495b2bb0ec6e3994033ca04e
Givriz <damien375@outlook.fr>
2020-11-06 17:52:27 +0100

    Update PluginAméliorationMinecraft

--- PluginAmeliorationMinecraft/src/fr/givrix/pluginameliorationminecraft/commands/CommandUhc.java
+++ PluginAmeliorationMinecraft/src/fr/givrix/pluginameliorationminecraft/commands/CommandUhc.java
@@ -1,5 +1,6 @@
 package fr.givrix.pluginameliorationminecraft.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Difficulty;
 import org.bukkit.GameRule;
 import org.bukkit.World;
@@ -19,19 +20,33 @@ public class CommandUhc implements CommandExecutor {
 			if (args.length == 0) {
 				World world = player.getWorld();
 				
-				if (world.getDifficulty() != Difficulty.HARD) {
-					world.setDifficulty(Difficulty.HARD);
+				if (world.getDifficulty() != Difficulty.HARD || world.getGameRuleValue(GameRule.NATURAL_REGENERATION) || !Bukkit.getServer().isHardcore()) {
+
+					if (world.getDifficulty() != Difficulty.HARD) {
+						world.setDifficulty(Difficulty.HARD);
+					}
+					
+					if (world.getGameRuleValue(GameRule.NATURAL_REGENERATION)) {
+						world.setGameRule(GameRule.NATURAL_REGENERATION, false);
+					}
+					
+					if (!Bukkit.getServer().isHardcore()) {
+						Bukkit.getServer().getWorld(player.getWorld().getName()).setHardcore(true);
+						Bukkit.broadcastMessage("§4Le serveur doit redémarrer pour changer votre mode de jeu");
+						try {
+							Thread.sleep(5000);
+						} catch (InterruptedException e) {
+							
+						}
+						Bukkit.getServer().spigot().restart();
+					}
+					
+					Bukkit.broadcastMessage("§6Votre mode de jeu a été mis à jour");
+					Bukkit.broadcastMessage("§eLe scénario UHC est maintenant activé. Ne perdez plus de vie !");
+					
+				} else {
+					player.sendMessage("Vous êtes déjà en UHC");
 				}
-				
-				/*if (!Bukkit.getServer().isHardcore()) {
-					Bukkit.getServer().getWorld(player.getWorld().getName()).setHardcore(true); // ne marche pas
-				}*/
-				
-				if (world.getGameRuleValue(GameRule.NATURAL_REGENERATION)) {
-					world.setGameRule(GameRule.NATURAL_REGENERATION, false);
-				}
-				
-				// dire à tout le monde que le mode de jeu a été mis à jour
 				
 			} else {
 				player.sendMessage("Syntaxe : /uhc");
